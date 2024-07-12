922b0a32f34e14c38378208ac1e17670c11d1c7f
ilyanovopashin <86349678+ilyanovopashin@users.noreply.github.com>
2024-07-12 12:29:57 +0400

    Add files via upload

---
+++ unith_con.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+app = Flask(__name__)
+HEADERS={'Content-Type': 'application/json'}
+BOT_WEBHOOK='https://admin.chatme.ai/connector/webim/webim_message/a1a4d7a9976dc3461271f0515d587ad0/bot_api_webhook'
+
+@app.route('/conversation', methods=['post'])
+def get_bot_answer():
+    incomingdata=request.json
+    print(incomingdata)
+    bot_response = requests.post(BOT_WEBHOOK, HEADERS, params = {'text': "event": "new_message", "chat": {"id": "some_id"},'text': incomingdata[0]['payload']['message']}).json()
+    resp = {[{"type": "text","payload": {"type": "text","message": bot_response['messages'][0]['text']}}]} 
+    return resp
+
+if __name__ == '__main__':
+    app.run()
