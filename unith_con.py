79764d51db7ab10a10136deea01f7e88435f13ca
ilyanovopashin <86349678+ilyanovopashin@users.noreply.github.com>
2024-07-12 14:32:54 +0400

    Add files via upload

--- unith_con.py
+++ unith_con.py
@@ -19,7 +19,6 @@ def get_bot_answer():
     print('json_bot_response_type = ', type(json_bot_response))
     print('json_bot_response= ', json_bot_response)
     print('json_bot_response_messages= ', json_bot_response['messages'])
-    print('json_bot_response.json= ', json_bot_response.json)
     resp = {[{'type': 'text', 'payload': {'type': 'text', 'message': json_bot_response.json['messages'][0]['text']}}]} 
     return resp
 
