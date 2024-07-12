267cb3513472b867463cdc136b96fbb6507fe986
ilyanovopashin <86349678+ilyanovopashin@users.noreply.github.com>
2024-07-12 13:43:03 +0400

    Add files via upload

--- unith_con.py
+++ unith_con.py
@@ -4,12 +4,14 @@ import requests
 app = Flask(__name__)
 HEADERS={'Content-Type': 'application/json'}
 BOT_WEBHOOK='https://admin.chatme.ai/connector/webim/webim_message/a1a4d7a9976dc3461271f0515d587ad0/bot_api_webhook'
+WEBHOOK_SITE='https://webhook.site/b03c80bf-6ae0-4222-bd81-4725376b81a8'
 
 @app.route('/conversation', methods=['post'])
 def get_bot_answer():
     incomingdata=request.json
     print('indata=', incomingdata)
     print('indatazero=', incomingdata[0]['payload']['message'])   
+    whs = requests.post(WEBHOOK_SITE, headers = HEADERS, json = {'event': 'new_message', 'chat': {'id': 'some_id'},'text': incomingdata[0]['payload']['message']}).json()
     bot_response = requests.post(BOT_WEBHOOK, headers = HEADERS, json = {'event': 'new_message', 'chat': {'id': 'some_id'},'text': incomingdata[0]['payload']['message']}).json()
     print('bot_response= ', bot_response)
     resp = {[{'type': 'text', 'payload': {'type': 'text', 'message': bot_response['messages'][0]['text']}}]} 
